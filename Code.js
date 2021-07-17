6bc0607fc970ad2ce3e7c1f7f5999ab5c46e4587
rishita-2008 <76506346+rishita-2008@users.noreply.github.com>
2021-07-17 16:18:48 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,27 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    
+    fill("white")
+    textSize(15)
+    text("U N F C O T I N", 700,150)
+    fill("lightblue")
+    text("Hint: Performs a partiuclar task!!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("A D A T S A B E", 100,250)
+    fill("lightblue")
+    text("Hint: Stores all information!!", 100,270)
+    
+
+}
